0adb9f550285e8d6bbbd062707de6f96708f9062
Shailendra Singh <75472269+ShailendraSingh16@users.noreply.github.com>
2023-10-04 10:19:40 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/ShailendraSingh16">Shailendra Singh</a>
                 </h4>
             </footer>
         </div>
